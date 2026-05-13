316f4d986e48b80d8491458ebbfcd00b588a7f7b
ElJony08 <jonvillafruela88@gmail.com>
2026-05-13 22:42:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function desvelarcontra(correoe){
 function contrasena(contra){
     
     if(contra == 'Macacopaga'){
-        open('https://www.canva.com', '_blank');
+        open('https://canva.link/u2loltmdwtnt1ak', '_blank');
     }else{
         contador = contador + 1
     }
